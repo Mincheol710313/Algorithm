9543b0a13cde3ae4aa67df52c988be031242981b
Mincheol710313 <99674522+Mincheol710313@users.noreply.github.com>
2024-03-08 02:08:19 +0900

    Add files via upload

---
+++ Time Complexity/24263.py
@@ -0,0 +1,14 @@
+"""
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # 코드1
+    return sum;
+}
+Input : A[]. n
+Output : A[] 안에 들어있는 원소의 값 중 n번째 요소까지의 합
+빅오표시법 : O(n)
+"""
+n = int(input())
+print(n)
+print(1)
