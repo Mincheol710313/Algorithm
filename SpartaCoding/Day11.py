61889b5ef0ecfb6012136b00904463a433dc4903
Mincheol710313 <99674522+Mincheol710313@users.noreply.github.com>
2024-11-07 19:39:21 +0900

    항해99 D+11

---
+++ SpartaCoding/Day11.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+N, M = map(int, input().split()) # N : Node 개수, M : 간선 개수
+
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    x, y = map(int, input().split())
+
+    graph[x].append(y)
+
+S = int(input())
+gomgom_list = list(map(int, input().split()))
+
+s = []
+s.append(1)
+flag = True
+while s:
+    v = s.pop()
+
+    if v in gomgom_list:
+        continue
+    else:
+        if len(graph[v]) == 0:
+            flag = False 
+        for idx in graph[v]:
+            s.append(idx)
+
+if flag:
+    print("Yes")
+else:
+    print("yes")
+    
