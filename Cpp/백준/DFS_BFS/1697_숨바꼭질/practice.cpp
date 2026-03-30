597a82ec6fbc8f15ac0e6fb6a87147b5c0ecfcc4
Mincheol710313 <mincheol710313@gmail.com>
2026-03-30 10:07:03 +0900

    [review/백준] 1697,2206. 숨바꼭질, 벽 부수고 이동하기 복습 추가

--- Cpp/백준/DFS_BFS/1697_숨바꼭질/practice.cpp
+++ Cpp/백준/DFS_BFS/1697_숨바꼭질/practice.cpp
@@ -3,41 +3,43 @@ using namespace std;
 
 #define MAX 100001
 
-int subin, brother;
+int subin, target;
 vector<int> dist;
 
-void bfs() {
+int bfs() {
     queue<int> q;
     q.push(subin);
+
     dist[subin] = 0;
 
     while (!q.empty()) {
-        int cur_pos = q.front(); 
+        int cur_pos = q.front();
         q.pop();
 
-        if (cur_pos == brother) {
-            cout << dist[brother];
-            break;
-        }
-        
-        for (int next : {cur_pos - 1, cur_pos + 1, cur_pos * 2}) {
-            if(next >= 0 && next < MAX && dist[next] == -1) {
-                dist[next] = dist[cur_pos] + 1;
-                q.push(next);
+        for(int next_pos : {cur_pos-1, cur_pos+1, cur_pos*2}) {
+            if (next_pos >= 0 && next_pos < MAX) {
+                if (next_pos == target) {
+                    dist[target] = dist[cur_pos] + 1;
+                    return dist[target];
+                }
+                else if (dist[next_pos] == -1) {
+                    dist[next_pos] = dist[cur_pos] + 1;
+                    q.push(next_pos);
+                }
             }
         }
-
     }
 }
+
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     // TODO: 풀이 작성
-    cin >> subin >> brother;
+    cin >> subin >> target;
     dist.assign(MAX, -1);
 
-    bfs();
+    cout << bfs();
 
     return 0;
 }
--- Cpp/백준/DFS_BFS/2206_벽 부수고 이동하기/practice.cpp
+++ Cpp/백준/DFS_BFS/2206_벽 부수고 이동하기/practice.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
@@ -8,36 +9,32 @@ vector<vector<int>> grid;
 vector<vector<vector<int>>> dist;
 
 bool inRange(int ny, int nx) {
-    return ny >= 0 && ny < y && nx >= 0 && nx < x;
+    return nx >= 0 && nx < x && ny >= 0 && ny < y;
 }
 
 int bfs() {
-    int result = -1;
-
-    dist[0][0][0] = 1;
     queue<tuple<int, int, int>> q;
     q.push({0,0,0});
 
+    dist[0][0][0] = 1;
+
     while (!q.empty()) {
-        auto [cur_y, cur_x, broke] = q.front(); 
-        q.pop();
+        auto [cur_y, cur_x, brake] = q.front(); q.pop();
 
         for (int i = 0; i < 4; i++) {
             int next_y = cur_y + dy[i];
             int next_x = cur_x + dx[i];
 
-            if (!inRange(next_y, next_x)) {
-                continue;
-            }
-            
-            if (broke == 0) {
+            if (!inRange(next_y, next_x)) continue;
+
+            if (brake == 0) {
                 if (grid[next_y][next_x] == 0 && dist[next_y][next_x][0] == -1) {
                     dist[next_y][next_x][0] = dist[cur_y][cur_x][0] + 1;
                     q.push({next_y, next_x, 0});
                 } else if (grid[next_y][next_x] == 1 && dist[next_y][next_x][1] == -1) {
                     dist[next_y][next_x][1] = dist[cur_y][cur_x][0] + 1;
                     q.push({next_y, next_x, 1});
-                }
+                }                 
             } else {
                 if (grid[next_y][next_x] == 0 && dist[next_y][next_x][1] == -1) {
                     dist[next_y][next_x][1] = dist[cur_y][cur_x][1] + 1;
@@ -47,13 +44,14 @@ int bfs() {
         }
     }
 
+    int result = -1;
     for (int i = 0; i < 2; i++) {
-        if(dist[y-1][x-1][i] != -1) {
-            if(result == -1) result = dist[y-1][x-1][i];
-            else result = min(result, dist[y-1][x-1][i]);
+        if (dist[y-1][x-1][i] != -1) {
+            if (result == -1) result = dist[y-1][x-1][i];
+            else result = min(dist[y-1][x-1][i], result);
         }
     }
-    
+
     return result;
 }
 
@@ -62,7 +60,7 @@ int main() {
     cin.tie(NULL);
     // TODO: 풀이 작성
     cin >> y >> x;
-    grid.assign(y, vector<int>(x));
+    grid.assign(y, vector<int>(x, 0));
     dist.assign(y, vector<vector<int>>(x, vector<int>(2, -1)));
 
     for (int i = 0; i < y; i++) {
@@ -73,7 +71,7 @@ int main() {
         }
     }
 
-    cout << bfs() << "\n";
+    cout << bfs();
 
     return 0;
 }
